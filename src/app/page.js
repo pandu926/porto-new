02db84e7df1331fca40afdcf09e70e5302aa6e4f
pandu926 <63030609+pandu926@users.noreply.github.com>
2024-01-02 08:02:19 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import Header from "@/components/Header";
 import Image from "next/image";
-import ParticleBackground from "@/components/ParticlesBg/ParticleBackground";
 
 import Content from "@/components/Content";
 import Services from "@/components/Services";
