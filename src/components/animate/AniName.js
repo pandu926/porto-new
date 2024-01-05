3c0a807f488a681af7be06887837c3242a154afa
pandu926 <63030609+pandu926@users.noreply.github.com>
2024-01-05 14:39:30 +0700

    ya

--- src/components/animate/AniName.js
+++ src/components/animate/AniName.js
@@ -5,28 +5,28 @@ import anime from "animejs";
 import Scrool from "./Scrool";
 
 export default function AniName() {
-  useEffect(() => {
-    anime
-      .timeline({ loop: true })
-      .add({
-        targets: ".ml15 .word",
-        scale: [14, 1],
-        opacity: [0, 1],
-        easing: "easeOutCirc",
-        duration: 800,
-        delay: (el, i) => 800 * i,
-      })
-      .add({
-        targets: ".ml15",
-        opacity: 0,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 20000,
-      });
-  }, []);
+  // useEffect(() => {
+  //   anime
+  //     .timeline({ loop: true })
+  //     .add({
+  //       targets: ".ml15 .word",
+  //       scale: [14, 1],
+  //       opacity: [0, 1],
+  //       easing: "easeOutCirc",
+  //       duration: 800,
+  //       delay: (el, i) => 800 * i,
+  //     })
+  //     .add({
+  //       targets: ".ml15",
+  //       opacity: 0,
+  //       duration: 1000,
+  //       easing: "easeOutExpo",
+  //       delay: 20000,
+  //     });
+  // }, []);
 
   return (
-    <div className="ml15 mx-32">
+    <div className="ml15 ">
       <div className="text-white capitalize text-center">
         <div className="text-xl md:text-3xl  font-bold md:font-extrabold mt-24">
           <p className="mt-5 word">hi ,</p>
